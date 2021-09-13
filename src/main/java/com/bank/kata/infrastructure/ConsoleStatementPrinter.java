c3ec962a573e70e7b7aa749872f91b6829dc27ea
LBE <lila.becha@gmail.com>
2021-09-13 22:02:43 +0200

    US 3 : add implementation for the console print statement

--- src/main/java/com/bank/kata/infrastructure/ConsoleStatementPrinter.java
+++ src/main/java/com/bank/kata/infrastructure/ConsoleStatementPrinter.java
@@ -3,13 +3,31 @@ package com.bank.kata.infrastructure;
 import com.bank.kata.domain.Transaction;
 import com.bank.kata.domain.port.StatementPrinter;
 
+import java.util.StringJoiner;
+
 /**
  * @author lila.becha
  */
 public class ConsoleStatementPrinter implements StatementPrinter {
 
+    private static final String SPERATOR = " | ";
+
     @Override
     public void print(Transaction transaction) {
-        throw new UnsupportedOperationException("The print operation is not implemented yet.");
+        System.out.println(prettifyStatementTransaction(transaction));
+    }
+
+    /**
+     * prettify statement transaction.
+     *
+     * @param transaction
+     * @return
+     */
+    private String prettifyStatementTransaction(Transaction transaction) {
+        StringJoiner stringJoiner = new StringJoiner(SPERATOR);
+        return stringJoiner.add("OperationType='" + transaction.transactionType() + "'")
+                .add("OperationDate='" + transaction.transactionDate() + "'")
+                .add("Amount='" + transaction.amount() + "'")
+                .add("Balance='" + transaction.balance() + "'").toString();
     }
 }
